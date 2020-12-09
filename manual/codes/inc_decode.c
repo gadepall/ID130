c50f29c99dd4c8b7564e8206d7e39b4242a9fea7
gadepall <gadepall@gmail.com>
2020-12-09 18:33:21 +0530

    Add existing files

--- manual/codes/inc_decode.c
+++ manual/codes/inc_decode.c
@@ -1,7 +1,7 @@
 //Code written on December 5, 2020
 //Revised  December 8, 2020
 // by G V V Sharma
-//This program implements a boolean function in C
+//This program implements the incremental decoder using boolean logic in C
 
 #include <stdio.h>
 
@@ -15,15 +15,18 @@ int main(void)
 
 //unsigned char takes input as 1 byte
 
-unsigned char  Z=0x00,Y=0x01,X=0x01,W=0x01;//inputs in hex	
+unsigned char  Z=0x01,Y=0x00,X=0x00,W=0x01;//inputs in hex	
 unsigned char one = 0x01;//used for displaying the output in bit
 unsigned char A,B,C,D;//outputs
 
 D = (W&X&Y&(~Z))|((~W)&(~X)&(~Y)&Z);//Boolean function for D
+B=((~Z)&(~Y)&(~X)&W)|((~Z)&(~Y)&X&(~W))|((~Z)&Y&(~X)&W)|((~Z)&Y&X&(~W));
+C=((~Z)&(~Y)&X&W)|((~Z)&Y&(~X)&(~W))|((~Z)&Y&(~Z)&W)|((~Z)&Y&X&(~W));
 A = ((~W)&(~X)&(~Y)&(~Z))|((~W)&(X)&(~Y)&(~Z))|((~W)&(~X)&Y&(~Z))|((~W)&X&Y&(~Z))|((~W)&(~X)&(~Y)&(Z));
 //Boolean function for A
 
-printf("%x\n",one&D);//Output D
-printf("%x\n",one&A);//Output D
+printf("%x%x%x%x",one&Z,one&Y,one&X,one&W);//Iutput ZYXW
+printf(" ");
+printf("%x%x%x%x\n" ,one&D,one&C,one&B,one&A);//Output DCBA
 return 0;
 }
